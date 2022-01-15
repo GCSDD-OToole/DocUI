99cea2eb6182a90447fdff676b5f5925868716e9
Brandon O'Toole <otoolb@gmail.com>
2022-01-15 00:07:13 -0800

    Pull actual code from files

--- scripts.js
+++ scripts.js
@@ -3,35 +3,133 @@ import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 window.addEventListener("DOMContentLoaded", setup);
 
 function setup() {
-    let button = document.getElementById("TryItButton");
-    button.onclick = buttonClick;
+    document.getElementById("AccessButton").onclick = accessClick;
+    document.getElementById("FileLoad").onclick = fileLoadClick;
+
+    let loadFileButton = document.getElementById("FileLoad");
+    loadFileButton.disabled = true;
+
+    let token = document.getElementById("PersonalAccessToken");
+    token.onchange = updateToken;
+    document.personalAccessToken = token.value;
+
+    let repoName = document.getElementById("RepoName");
+    repoName.onchange = updateRepoName;
+    document.repoName = repoName.value;
 }
 
-async function buttonClick() {
-    let personalAccessToken = document.getElementById("PersonalAccessToken").value;
+function updateToken() {
+    let token = document.getElementById("PersonalAccessToken");
+    document.personalAccessToken = token.value;
+    document.octokit = undefined;
+    document.username = undefined;
 
-    if (personalAccessToken == "") {
+    let loadFileButton = document.getElementById("FileLoad");
+    loadFileButton.disabled = true;
+}
+
+function updateRepoName() {
+    let repoName = document.getElementById("RepoName");
+    document.repoName = repoName.value;
+
+    let loadFileButton = document.getElementById("FileLoad");
+    loadFileButton.disabled = true;
+}
+
+async function fileLoadClick() {
+    let username = document.username;
+    let repoName = document.repoName;
+
+    // get the selected option
+    let select = document.getElementById("FileSelect");
+    let option = select.options[select.selectedIndex];
+
+    // if the selected option has a .h
+    if (option.text.includes(".h")) {
+        let filename = option.value + ".h";
+        let contents = await getFileContents(username, repoName, filename);
+        document.getElementById("hContent").innerHTML = contents;
+    }
+
+    // if the selected option has a .cpp
+    if (option.text.includes(".cpp")) {
+        let filename = option.value + ".cpp";
+        let contents = await getFileContents(username, repoName, filename);
+        document.getElementById("cppContent").innerHTML = contents;
+    }
+
+}
+
+async function accessClick() {
+    if (document.personalAccessToken == "") {
         document.getElementById("debugoutput").innerHTML = "Please enter a Personal Access Token";
         return;
     }
 
-    document.octokit = new Octokit({
-        auth: personalAccessToken
+    document.octokit = document.octokit || new Octokit({
+        auth: document.personalAccessToken
     });
 
-    let RepoName = document.getElementById("RepoName").value;
+    document.username = document.username || await getUsername();
 
-    const username = await getUsername();
-
-    if (RepoName == "") {
+    if (document.repoName == "") {
         document.getElementById("debugoutput").innerHTML = "Please enter a repo name";
         return;
     }
 
-    const files = await getFileList(RepoName, username);
-    const headers = files.map(x=>makeHeaderComment(username, x)).join("<br><br>");
+    await processFiles();
+}
 
-    document.getElementById("debugoutput").innerHTML = headers;
+async function processFiles() {
+    // download a list of files
+    const files = await getFileList(document.repoName, document.username);
+
+    // match cpp and h files
+    let fileTypes = {};
+
+    for (let file of files) {
+        if (file.endsWith(".cpp")) {
+            let fileBase = file.slice(0, -4);
+            fileTypes[fileBase] = fileTypes[fileBase] || [];
+
+            fileTypes[fileBase].push(".cpp");
+        } else if (file.endsWith(".h")) {
+            let fileBase = file.slice(0, -2);
+            fileTypes[fileBase] = fileTypes[fileBase] || [];
+
+            fileTypes[fileBase].push(".h");
+        }
+    }
+
+    // update the dropdown menu
+    let selectBox = document.getElementById("FileSelect");
+    selectBox.options.length = 0;
+
+    for (const [key, value] of Object.entries(fileTypes)) {
+        let option = document.createElement("option");
+        option.text = key + " (" + value + ")";
+        option.value = key;
+        selectBox.add(option);
+    }
+
+    // enable the load button
+    let loadFileButton = document.getElementById("FileLoad");
+    loadFileButton.disabled = false;
+
+
+
+
+
+    //const headers = files.map(x=>makeHeaderComment(username, x)).join("<br><br>");
+
+    //document.getElementById("debugoutput").innerHTML = headers;
+}
+
+function arrayToString(a, ) {
+}
+
+function fileSelect(file) {
+    return '<option value="' + file + '">' + file + "</option>";
 }
 
 function makeHeaderComment(username, filename) {
