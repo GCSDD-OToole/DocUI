bb885657f05917940697f13d698a6c9abb398180
Brandon O'Toole <otoolb@gmail.com>
2022-01-17 13:14:27 -0800

    Add Commit Email to settings

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -5,48 +5,44 @@ function setup() {
 
     let savedRepo = localStorage.getItem('savedRepo');
     let savedName = localStorage.getItem('savedName');
+    let savedEmail = localStorage.getItem('savedEmail');
     let savedNumber = localStorage.getItem('savedNumber');
     let savedDate = localStorage.getItem('savedDate');
 
     document.getElementById("RepoName").value = savedRepo;
     document.getElementById("StudentName").value = savedName;
+    document.getElementById("CommitEmail").value = savedEmail;
     document.getElementById("StudentNumber").value = savedNumber;
     document.getElementById("DueDate").value = savedDate;
 
     document.getElementById("SaveRepo").onclick = saveRepo;
     document.getElementById("SaveName").onclick = saveName;
+    document.getElementById("SaveEmail").onclick = saveEmail;
     document.getElementById("SaveNumber").onclick = saveNumber;
     document.getElementById("SaveDate").onclick = saveDate;
 }
 
 function saveDate() {
-    let localStorage = window.localStorage;
-
     let value = document.getElementById("DueDate").value;
-    console.log(value);
-    localStorage.setItem('savedDate', value);
+    window.localStorage.setItem('savedDate', value);
 }
 
 function saveName() {
-    let localStorage = window.localStorage;
-
     let value = document.getElementById("StudentName").value;
-    console.log(value);
-    localStorage.setItem('savedName', value);
+    window.localStorage.setItem('savedName', value);
+}
+
+function saveEmail() {
+    let value = document.getElementById("CommitEmail").value;
+    window.localStorage.setItem('savedEmail', value);
 }
 
 function saveNumber() {
-    let localStorage = window.localStorage;
-
     let value = document.getElementById("StudentNumber").value;
-    console.log(value);
-    localStorage.setItem('savedNumber', value);
+    window.localStorage.setItem('savedNumber', value);
 }
 
 function saveRepo() {
-    let localStorage = window.localStorage;
-
     let value = document.getElementById("RepoName").value;
-    console.log(value);
-    localStorage.setItem('savedRepo', value);
+    window.localStorage.setItem('savedRepo', value);
 }
